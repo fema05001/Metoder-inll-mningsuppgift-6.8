7666c0c380ab46830c751390b292a6bf9777b561
Elev <Elev@TEB125102>
2023-03-22 09:29:11 +0100

    Add project files.

---
+++ Metoder inllämningsuppgift 6.8/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace metoder
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv ett valfrit heltal");
+            string a = Console.ReadLine();
+            int b = int.Parse(a);
+            Console.WriteLine(" ");
+            
+            Console.WriteLine("Talet " + b + " har " + Primtal(b) +" Primtal unde sig");
+        }
+        static int Primtal(int tal)
+        {
+            int g = 0;
+            
+            for (int i = 0; i < tal; i++)
+            {
+                if (i % 3 == 0 & i !=3 || i % 2 == 0 & i !=2 || i % 5 ==0 & i!=5 || i % 7 ==0 & i !=7)
+                {
+                    continue;
+                    
+                }
+
+                else if (i % 3 != 0 || i == 3 & i % 2 != 0 || i == 2)
+                {
+                    g++;
+                    Console.WriteLine(i);
+                }
+               
+            }
+            return g;
+            return 0;
+        }
+    }
+}
